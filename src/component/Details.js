e95e83152ea216b5137ce0d4850670579fdc3fbf
vivek kumar <vivekumar190@gmail.com>
2023-01-23 23:57:25 +0530

    all things working

---
+++ src/component/Details.js
@@ -0,0 +1,99 @@
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+
+const Details = () => {
+  const params = useParams();
+  const [data, seTdATA] = useState();
+  const fetchData = () => {
+    axios
+      .get(`https://jsonplaceholder.typicode.com/users/${params.id}`)
+      .then((response) => {
+        seTdATA(response.data);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+  useEffect(() => {
+    fetchData();
+  }, []);
+  console.log(data);
+  return (
+    <div className="wrapper">
+      <div className="container h-100">
+        <div className="headerTitle">Company Hub</div>
+        <div className="Header">Company List</div>
+        <div className="row card">
+          <div className="col-12">
+            <div className="imagecolumn">
+              {" "}
+              <img src="/icons/company.png" height={"auto"} width={60}></img>
+              <div className="column">
+                <div className="cardtitle">{data?.company?.name}</div>
+                <div className="fieldvalue">{data?.company?.catchPhrase}</div>
+                <div className="fieldvaluebs">{data?.company?.bs}</div>
+              </div>
+            </div>
+          </div>
+          <div className="col-md-3 col-lg-3 col-sm-6">
+            <div className="column">
+              Name
+              <div className="fieldvalue">{data?.name}</div>
+            </div>
+          </div>
+          <div className="col-md-3 col-lg-3 col-sm-6">
+            <div className="column">
+              Phone
+              <div className="fieldvalue">{data?.phone}</div>
+            </div>
+          </div>
+          <div className="col-md-3 col-lg-3 col-sm-6">
+            <div className="column">
+              Username
+              <div className="fieldvalue">{data?.username}</div>
+            </div>
+          </div>
+          <div className="col-md-3 col-lg-3 col-sm-6">
+            <div className="column">
+              Email
+              <div className="fieldvalue">{data?.email}</div>
+            </div>
+          </div>
+          <div className="col-md-3 col-lg-3 col-sm-6">
+            <div className="column">
+              Website
+              <div className="fieldvalue">{data?.website}</div>
+            </div>
+          </div>
+          <div className="col-md-3 col-lg-3 col-sm-6">
+            <div className="column">
+              Email
+              <div className="fieldvalue">{data?.email}</div>
+            </div>
+          </div>
+          <div className="col">
+            <div className="column">
+              Street
+              <div className="fieldvalue">{data?.address?.street}</div>
+            </div>
+          </div>
+          <div className="col">
+            <div className="column">
+              city
+              <div className="fieldvalue">{data?.address?.city}</div>
+            </div>
+          </div>
+          <div className="col">
+            <div className="column">
+              Zip Code
+              <div className="fieldvalue">{data?.address?.zipcode}</div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Details;
---
+++ src/component/HomeScreen.js
@@ -0,0 +1,76 @@
+import React, { useEffect, useState } from "react";
+import "../App.css";
+import axios from "axios";
+import { Link, Navigate } from "react-router-dom";
+
+const HomeScreen = () => {
+  const [data, seTdATA] = useState();
+  const fetchData = () => {
+    axios
+      .get("https://jsonplaceholder.typicode.com/users")
+      .then((response) => {
+        seTdATA(response.data);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+  useEffect(() => {
+    fetchData();
+  }, []);
+  console.log(data);
+
+  const Naviaget = () => {
+    return;
+  };
+  return (
+    <div className="App">
+      <div className="container">
+        <div className="headerTitle">Company Hub</div>
+        <div className="Header">Company List</div>
+        <div className="row g-3">
+          {data?.map((company) => (
+            <div className="col-12">
+              <div className="card">
+                <div className="imagecolumn">
+                  {" "}
+                  <img
+                    src="/icons/company.png"
+                    height={"auto"}
+                    width={60}
+                  ></img>
+                  <div className="column">
+                    <div className="cardtitle">{company?.company?.name}</div>
+                    <div className="fieldvalue">
+                      {company?.company?.catchPhrase}
+                    </div>
+                  </div>
+                </div>
+
+                <div className="middlerow">
+                  <div className="column">
+                    Contact
+                    <div className="fieldvalue">{company?.name}</div>
+                  </div>
+                  <div className="column">
+                    Street
+                    <div className="fieldvalue">{company?.address?.street}</div>
+                  </div>
+                  <div className="column">
+                    City
+                    <div className="fieldvalue">{company?.address?.city}</div>
+                  </div>
+                </div>
+                <Link to={`${company?.id}`}>
+                  <button className="detailsbutton">View details</button>
+                </Link>
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default HomeScreen;
